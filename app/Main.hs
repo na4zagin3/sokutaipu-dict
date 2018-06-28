b7566d8b521effdac1230a25eefccb37f48ccdd7
na4zagin3 <mrty.ityt.pt@gmail.com>
2018-06-28 23:23:19 +0900

    Remove Lib

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-import Lib
 import Stenotype.Sokutype.Prim
 import Stenotype.Sokutype.Generate
 
@@ -12,5 +11,4 @@ main :: IO ()
 main = do
   let duplicatedStrokes = M.filter (\xs -> length xs > 1) strokeTable
   print $ length strokeTable
-  _ <- mapM_ putStrLn $ map (\(s, sps) -> showStroke s ++ ": " ++ L.intercalate ", " (map fst $ S.toList sps)) $ M.toList duplicatedStrokes
-  someFunc
+  mapM_ putStrLn $ map (\(s, sps) -> showStroke s ++ ": " ++ L.intercalate ", " (map fst $ S.toList sps)) $ M.toList duplicatedStrokes
