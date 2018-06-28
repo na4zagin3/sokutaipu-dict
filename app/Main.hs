43e6a30ea9237590d8e58d1490974d240e596292
na4zagin3 <mrty.ityt.pt@gmail.com>
2018-06-28 23:53:39 +0900

    Output keystoroke dictionary to stdout

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
+import qualified Data.Aeson as J
+import qualified Data.ByteString.Lazy.Char8 as BL8
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
+import System.IO
 
 import Stenotype.Sokutype.Prim
 import Stenotype.Sokutype.Generate
@@ -10,5 +13,11 @@ import Stenotype.Sokutype.Generate
 main :: IO ()
 main = do
   let duplicatedStrokes = M.filter (\xs -> length xs > 1) strokeTable
-  print $ length strokeTable
-  mapM_ putStrLn $ map (\(s, sps) -> showStroke s ++ ": " ++ L.intercalate ", " (map fst $ S.toList sps)) $ M.toList duplicatedStrokes
+  hPutStrLn stderr $ "Total keystrokes"
+  hPutStrLn stderr . show $ length strokeTable
+  hPutStrLn stderr $ "Duplicated keystrokes"
+  hPutStrLn stderr . show $ length duplicatedStrokes
+  mapM_ (hPutStrLn stderr) $ map (\(s, sps) -> showStroke s ++ ": " ++ L.intercalate ", " (map fst $ S.toList sps)) $ M.toList duplicatedStrokes
+  let f s | S.null s = Nothing
+          | otherwise = Just . fst $ S.elemAt 0 s
+  BL8.putStrLn $ J.encode $ M.mapKeys showStroke $ M.mapMaybe f strokeTable
